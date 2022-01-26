3e085783b2cf143204fb20626cecc96e18d516ed
r.polunin@buro247.ru <es54P25eseg74>
2022-01-26 14:32:12 +0300

    Компонент кнопки

--- develop/src/components/ui/button/index.vue
+++ develop/src/components/ui/button/index.vue
@@ -1,11 +1,59 @@
 <template>
-	<div class="ui-button"></div>
+	<BaseLink
+		class="ui-button"
+		v-if="isLink"
+		v-bind="$attrs"
+	>
+		<slot></slot>
+	</BaseLink>
+	<button
+		class="ui-button"
+		v-else
+	>
+		<slot></slot>
+	</button>
 </template>
 
 <script>
-export default {
-	name: 'UiButton',
-}
+	import BaseLink from '../../base/link/index.vue';
+
+	const templates = [
+		'default',
+		'ghost',
+		'text',
+	];
+	const templateDefault = templates[0];
+
+	export default {
+		name: 'UiButton',
+		inheritAttrs: false,
+
+		components: {
+			BaseLink,
+		},
+
+		props: {
+			template: {
+				type: String,
+				default: templateDefault,
+				validator(value) {
+					return templates.indexOf(value) >= 0;
+				},
+			},
+		},
+
+		computed: {
+			rootClass() {
+				return [
+					`_template_${this.template}`,
+				];
+			},
+
+			isLink() {
+				return !!this.$attrs.to;
+			},
+		},
+	}
 </script>
 
 <style lang="scss">
