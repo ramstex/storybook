744e601c46bc5a8b1d5cf2103f2d13f35d0cdf43
r.polunin@buro247.ru <es54P25eseg74>
2022-01-26 19:17:12 +0300

    Кнопки на разных фонах

--- develop/src/pages/storybook/index.vue
+++ develop/src/pages/storybook/index.vue
@@ -1,19 +1,15 @@
 <template>
 	<div class="page-storybook">
-		<MarkupContainer wide>
+		<MarkupSection class="page-storybook__section">
 			<h1>Storybook page</h1>
+		</MarkupSection>
+		<MarkupSection class="page-storybook__section">
 			<div class="page-storybook__row">
 				<div class="page-storybook__col">
 					<UiButton>Обычная кнопка</UiButton>
 				</div>
 				<div class="page-storybook__col">
-					<UiButton invert>Инвертированная кнопка</UiButton>
-				</div>
-				<div class="page-storybook__col">
 					<UiButton ghost>Кнопка-призрак</UiButton>
-				</div>
-				<div class="page-storybook__col">
-					<UiButton invert ghost>Инвертированная кнопка-призрак</UiButton>
 				</div>
 			</div>
 			<div class="page-storybook__row">
@@ -21,13 +17,7 @@
 					<UiButton theme="primary">Primary кнопка</UiButton>
 				</div>
 				<div class="page-storybook__col">
-					<UiButton theme="primary" invert>Инвертированная Primary кнопка</UiButton>
-				</div>
-				<div class="page-storybook__col">
 					<UiButton theme="primary" ghost>Кнопка-призрак Primary</UiButton>
-				</div>
-				<div class="page-storybook__col">
-					<UiButton theme="primary" invert ghost>Инвертированная кнопка-призрак Primary</UiButton>
 				</div>
 			</div>
 			<div class="page-storybook__row">
@@ -35,10 +25,35 @@
 					<UiButton theme="secondary">Secondary кнопка</UiButton>
 				</div>
 				<div class="page-storybook__col">
-					<UiButton theme="secondary" invert>Инвертированная Secondary кнопка</UiButton>
+					<UiButton theme="secondary" ghost>Кнопка-призрак Secondary</UiButton>
+				</div>
+			</div>
+			<div class="page-storybook__row">
+				<div class="page-storybook__col _full">
+					<UiButton full>Кнопка на всю ширину</UiButton>
+				</div>
+			</div>
+		</MarkupSection>
+		<MarkupSection class="page-storybook__section _inverse">
+			<div class="page-storybook__row">
+				<div class="page-storybook__col">
+					<UiButton invert>Инвертированная кнопка</UiButton>
 				</div>
 				<div class="page-storybook__col">
-					<UiButton theme="secondary" ghost>Кнопка-призрак Secondary</UiButton>
+					<UiButton invert ghost>Инвертированная кнопка-призрак</UiButton>
+				</div>
+			</div>
+			<div class="page-storybook__row">
+				<div class="page-storybook__col">
+					<UiButton theme="primary" invert>Инвертированная Primary кнопка</UiButton>
+				</div>
+				<div class="page-storybook__col">
+					<UiButton theme="primary" invert ghost>Инвертированная кнопка-призрак Primary</UiButton>
+				</div>
+			</div>
+			<div class="page-storybook__row">
+				<div class="page-storybook__col">
+					<UiButton theme="secondary" invert>Инвертированная Secondary кнопка</UiButton>
 				</div>
 				<div class="page-storybook__col">
 					<UiButton theme="secondary" invert ghost>Инвертированная кнопка-призрак Secondary</UiButton>
@@ -46,23 +61,23 @@
 			</div>
 			<div class="page-storybook__row">
 				<div class="page-storybook__col _full">
-					<UiButton full>Обычная кнопка на всю ширину</UiButton>
+					<UiButton full ghost theme="primary">Кнопка на всю ширину</UiButton>
 				</div>
 			</div>
-		</MarkupContainer>
+		</MarkupSection>
 	</div>
 </template>
 
 <script>
 	import UiButton from '../../../src/components/ui/button/index.vue';
-	import MarkupContainer from '../../../src/components/markup/container/index.vue';
+	import MarkupSection from '../../../src/components/markup/section/index.vue';
 
 	export default {
 		name: 'PageStorybook',
 
 		components: {
 			UiButton,
-			MarkupContainer,
+			MarkupSection,
 		},
 
 		methods: {
@@ -92,6 +107,18 @@
 
 			&._full {
 				width: 100%;
+			}
+		}
+
+		&__section {
+			& + & {
+				margin-top: $gutter*2;
+			}
+
+			&._inverse {
+				padding-top: $gutter * 2;
+				padding-bottom: $gutter * 2;
+				background-color: var(--color-back--invert);
 			}
 		}
 	}
