e5d96a1d989934362706cee1b5e832f796816f79
r.polunin@buro247.ru <es54P25eseg74>
2022-01-26 15:18:13 +0300

    Страница storybook с кнопками

--- develop/src/pages/storybook/index.vue
+++ develop/src/pages/storybook/index.vue
@@ -2,17 +2,24 @@
 	<div class="page-storybook">
 		<MarkupContainer>
 			<h1>Storybook page</h1>
+			<div class="page-storybook__row">
+				<div class="page-storybook__col">
+					<UiButton>Обычная кнопка</UiButton>
+				</div>
+			</div>
 		</MarkupContainer>
 	</div>
 </template>
 
 <script>
+	import UiButton from '../../../src/components/ui/button/index.vue';
 	import MarkupContainer from '../../../src/components/markup/container/index.vue';
 
 	export default {
 		name: 'PageStorybook',
 
 		components: {
+			UiButton,
 			MarkupContainer,
 		},
 
@@ -23,7 +30,27 @@
 </script>
 
 <style lang="scss">
-	.page-storybook {
+	@import '../../../src/style/globals/src.scss';
 
+	.page-storybook {
+		&__row {
+			display: flex;
+			flex-wrap: wrap;
+			margin-left: -$gutter*0.5;
+			margin-right: -$gutter*0.5;
+
+			&:not(:first-child) {
+				margin-top: $gutter;
+			}
+		}
+
+		&__col {
+			padding-left: $gutter*0.5;
+			padding-right: $gutter*0.5;
+
+			&._full {
+				width: 100%;
+			}
+		}
 	}
 </style>
