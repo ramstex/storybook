0c752187068bfa43f7fe202efd876dbb8d631aec
roman <polunin@alkor.co.ru>
2022-01-27 14:14:13 +0300

    markup-grid

---
+++ develop/src/components/markup/grid/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="markup-grid">
+		<slot />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'MarkupGrid',
+	}
+</script>
+
+<style lang="scss">
+	.markup-grid {
+
+	}
+</style>
