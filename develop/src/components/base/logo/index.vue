d64f459fd73231a9f709ea8ba9e478d4b794cffd
roman <polunin@alkor.co.ru>
2022-01-27 12:42:13 +0300

    base-logo component

---
+++ develop/src/components/base/logo/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<BaseLink
+		class="base-logo"
+		v-bind="$attrs"
+		@click="onClick"
+	>
+		<img
+			class="base-logo__image"
+			:src="src"
+			:alt="alt"
+			:title="title"
+		/>
+	</BaseLink>
+</template>
+
+<script>
+	import BaseLink from '../link/index.vue';
+	export default {
+		name: 'BaseLogo',
+		inheritAttrs: false,
+
+		components: {
+			BaseLink,
+		},
+
+		props: {
+			src: {
+				type: String,
+				required: true,
+			},
+
+			alt: {
+				type: String,
+			},
+
+			title: {
+				type: String,
+			},
+		},
+
+		methods: {
+			onClick() {
+				this.$emit('click');
+			},onClick
+		},
+	}
+</script>
+
+<style lang="scss">
+	.base-logo {
+		display: block;
+
+		&__image {
+			width: 100%;
+		}
+	}
+</style>
